51db72074671b2544976aefa9e3bd2fdee9e97da
Calvin Remsburg <cremsburg@protonmail.com>
2021-08-13 10:55:24 +0000

    add in address-form

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AddressFormComponent } from './address-form/address-form.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'address-form',
+    component: AddressFormComponent
+  },
+  {
+    path: '',
+    redirectTo: '/address-form',
+    pathMatch: 'full'
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
